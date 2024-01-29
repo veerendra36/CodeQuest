ba4f8421a1a85872fb4fa694a41f6f9f46cd0af7
veerendra <veerendra.20mis7039@vitap.ac.in>
2024-01-29 15:43:39 +0530

    this is test 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">hello world</h1>
+      <h1 className="h1-bold">hello world&apos;s</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      /// test
       screens: {
         xs: "420px",
       },
